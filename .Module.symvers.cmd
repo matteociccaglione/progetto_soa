559d01898896fc7de467109c1beb665e99503af5
Matteo <matteoc8@live.com>
2023-08-22 10:06:16 +0200

    empty

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/media/sf_progetto_soa/Module.symvers :=  sed 's/ko$$/o/'  /media/sf_progetto_soa/modules.order | scripts/mod/modpost -m     -o /media/sf_progetto_soa/Module.symvers -e -i Module.symvers -T - 
